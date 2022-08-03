272a73fed9b43bf4ce37222c7aefd17a88f594c9
Santi <santiago.l.bratschi@gmail.com>
2022-08-03 00:54:33 -0300

    boss_changes

--- Assets/Scripts/Enemies/Enemy_4/Boss.cs
+++ Assets/Scripts/Enemies/Enemy_4/Boss.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    
-    //Boss Movement
+
+    PlayerStats playerStats;
+    private AudioSource _myAudioSource;
+
+    //Boss Base
     private float RotateSpeed = 2.5f;
     private float Radius = 0.25f;
+    public float bossMaxLife = 100;
+    public float bossCurrentLife = 0;
+    
 
     private Vector2 _centre;
     private float _angle;
 
     //Boss Ray
-    //public LineRenderer lineRenderer;
-    //public Transform firePoint;
+    public LineRenderer lineRenderer;
+    public Transform firePoint;
+    public Transform gaia;
+    public float shootCount = 3;
+    public int rayDamage = 5;
+    
 
     private void Start()
     {
         _centre = transform.position;
         DisableLaser();
+        bossCurrentLife = bossMaxLife;
     }
 
     private void Update()
@@ -30,16 +41,68 @@ public class Boss : MonoBehaviour
         var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
         transform.position = _centre + offset;
 
-
+        shootCount -= Time.deltaTime;
+        if (shootCount <= 0)
+        {
+            StartCoroutine(ShootBoss());
+        }
     }
 
     void EnableLaser()
     {
-        //lineRenderer.enabled = true;
+        lineRenderer.enabled = true;
     }
     
     void DisableLaser()
     {
-        //lineRenderer.enabled = false;
+        lineRenderer.enabled = false;
+    }
+
+    private IEnumerator ShootBoss()
+    {
+        yield return new WaitForSeconds(1.5f);
+        RotateSpeed = 0;
+        yield return new WaitForSeconds(0.2f);
+        EnableLaser();
+        lineRenderer.SetPosition(0, firePoint.position);
+        lineRenderer.SetPosition(1, gaia.position);
+        yield return new WaitForSeconds(2f);
+        DisableLaser();
+        yield return new WaitForSeconds(0.2f);
+        RotateSpeed = 2.5f;
+        shootCount = 3;
+    }
+
+    void Damage()
+    {
+        playerStats.currentHealth -= rayDamage;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Damage();
+        }
+    }
+
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            //if (_myAudioSource != null)
+            //{
+            //    _myAudioSource.Play();
+            //}
+            //else
+            //{
+            //    _myAudioSource = gameObject.GetComponentInChildren<AudioSource>();
+            //    _myAudioSource.Play();
+            //}
+
+            bossCurrentLife -= 10;
+          
+        }
+        
     }
 }
